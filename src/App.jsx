84952f6e6d846686b1b1178de1e3eb630b79b5aa
Lucas Gabriel <lucasgabrielgomes33@gmail.com>
2025-12-18 18:00:18 -0300

    refactor: correção de nome do arquivo ListaTarefas.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Cabecalho from './components/Cabecalho'
 import NovaTarefa from './components/NovaTarefa';
-import ListaTarefas from './components/ListaTarefas ';
+import ListaTarefas from './components/ListaTarefas';
 
 function App() {
   const [tarefas, setTarefas] = useState([
