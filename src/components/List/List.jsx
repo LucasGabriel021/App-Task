ca142245171ec777db47eadbeda8e9609607308f
Lucas Gabriel <lucasgabrielgomes33@gmail.com>
2025-12-23 12:08:38 -0300

    feat: criação de componentes da lista dinamicamente

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import ItemList from '../ItemList/ItemList'
+import { TasksContext } from '../../App'
 
 export default function List() {
+  const [tasks] = useContext(TasksContext);
+
   return (
-    <div className='app-container mt-2'>
-        <ItemList texto='Estudar react native'/>
+    <div className='app-container mt-2 flex flex-col gap-y-2'>
+        {
+          tasks.map((item) => {
+            return <ItemList texto={item.title}/>
+          })
+        }
     </div>
   )
 }
--- src/components/SectionTask/SectionTask.jsx
+++ src/components/SectionTask/SectionTask.jsx
@@ -1,26 +1,28 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import MessageEmpty from '../MessageEmpty/MessageEmpty'
 import List from '../List/List'
+import { TasksContext } from '../../App'
 
 export default function SectionTask() {
-  return (
-    <section className='flex flex-col items-center'>
-        <div className='app-container flex justify-between'>
-            <div className='flex gap-x-2 items-center'>
-                <h4 className='text-blue-400 font-bold text-sm'>Tarefas criadas</h4>
-                <span className='bg-gray-600 rounded-lg px-2 py-1'>
-                    <h4 className='text-slate-50 font-bold text-sm'>0</h4>
-                </span>
+    const [ tasks ] = useContext(TasksContext);
+
+    return (
+        <section className='flex flex-col items-center'>
+            <div className='app-container flex justify-between'>
+                <div className='flex gap-x-2 items-center'>
+                    <h4 className='text-blue-400 font-bold text-sm'>Tarefas criadas</h4>
+                    <span className='bg-gray-600 rounded-lg px-2 py-1'>
+                        <h4 className='text-slate-50 font-bold text-sm'>{tasks.length > 0 ? tasks.length : 0}</h4>
+                    </span>
+                </div>
+                <div className='flex gap-x-2 items-center'>
+                    <h4 className='text-blue-400 font-bold text-sm'>Concluídas</h4>
+                    <span className='bg-gray-600 rounded-lg px-2 py-1'>
+                        <h4 className='text-slate-50 font-bold text-sm'>0</h4>
+                    </span>
+                </div>
             </div>
-            <div className='flex gap-x-2 items-center'>
-                <h4 className='text-blue-400 font-bold text-sm'>Concluídas</h4>
-                <span className='bg-gray-600 rounded-lg px-2 py-1'>
-                    <h4 className='text-slate-50 font-bold text-sm'>0</h4>
-                </span>
-            </div>
-        </div>
-        <MessageEmpty/>
-        <List/>
-    </section>
-  )
+            {tasks.length != 0 ? <List /> : <MessageEmpty />}   
+        </section>
+    )
 }
